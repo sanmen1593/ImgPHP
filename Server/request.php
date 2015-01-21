14d117966b035e9799704aa02ad5a045c500023b
Santiago Mendoza Ramirez <contacto@santiagomendoza.org>
2015-01-21 11:24:22 -0500

    Si no vienen datos, no hacer nada
    Si no vienen los datos, no haga mas nada, simplemente responda el status
    code 500.

--- Server/request.php
+++ Server/request.php
@@ -1,32 +1,32 @@
 <?php
 
 $allsaved = true;
-/*Una variable para saber si se guardaron todas las imagenes o no
-En caso de que no, se envia un status_code 500 y tienen que mandarse las imagenes de nuevo.
+/* Una variable para saber si se guardaron todas las imagenes o no
+  En caso de que no, se envia un status_code 500 y tienen que mandarse las imagenes de nuevo.
  */
 
 
 if (isset($_POST["data"])) {
     $datos = $_POST["data"];
+    foreach ($datos as $file) {
+        var_dump(count($datos));
+        if (isset($file['img']) && isset($file['filename'])) {
+            $encodedimage = $file['img'];
+            $filename = $file['filename'];
+        } else {
+            $allsaved = false;
+        }
+        try {
+            $decodedimage = base64_decode($encodedimage);
+            file_put_contents($filename, $decodedimage);
+        } catch (Error $e) {
+            $allsaved = false;
+        }
+    }
 } else {
     $allsaved = false;
 }
 
-foreach ($datos as $file) {
-    var_dump(count($datos));
-    if (isset($file['img']) && isset($file['filename'])) {
-        $encodedimage = $file['img'];
-        $filename = $file['filename'];
-    } else {
-        $allsaved = false;
-    }
-    try {
-        $decodedimage = base64_decode($encodedimage);
-        file_put_contents($filename, $decodedimage);
-    } catch (Error $e) {
-        $allsaved = false;
-    }
-}
 if ($allsaved == false) {
     http_response_code(500);
 } else {
